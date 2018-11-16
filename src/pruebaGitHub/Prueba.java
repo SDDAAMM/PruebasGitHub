cbaa13f044c030001d51187106c32b62623b7b8b
Samuel <samueljimenezluis@gmail.com>
2018-11-16 10:28:08 +0100

    Module-info eliminado

--- src/pruebaGitHub/Prueba.java
+++ src/pruebaGitHub/Prueba.java
@@ -4,7 +4,7 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        int a=4, c=6;
+        int a=4, c=7;
         System.out.println("Test 1 ="+ (a+c));
 	}
 
