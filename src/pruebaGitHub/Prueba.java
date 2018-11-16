b857371f369e60fdc2754dc87b97e5faf3869aaf
Samuel <samueljimenezluis@gmail.com>
2018-11-16 09:59:24 +0100

    Primera fase del testeo

---
+++ src/pruebaGitHub/Prueba.java
@@ -0,0 +1,11 @@
+package pruebaGitHub;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a=4, c=6;
+        System.out.println("Test 1 ="+ (a+c));
+	}
+
+}
