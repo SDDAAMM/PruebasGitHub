617e2f7fba32a138fa6edf07d2009d4adb089186
Samuel <samueljimenezluis@gmail.com>
2018-11-19 10:15:54 +0100

    Sgiguiente

--- src/pruebaGitHub/Prueba.java
+++ src/pruebaGitHub/Prueba.java
@@ -4,7 +4,7 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        int a=4, c=7;
+        int a=4, c=15;
         System.out.println("Test 1 ="+ (a+c));
 	}
 
