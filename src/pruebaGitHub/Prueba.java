d46acc4a37b6f12363adfc1197b1a45f4c1d38d1
Samuel <samueljimenezluis@gmail.com>
2018-11-19 10:29:24 +0100

    Añadido Else

--- src/pruebaGitHub/Prueba.java
+++ src/pruebaGitHub/Prueba.java
@@ -4,8 +4,13 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        int a=4, c=6;
+        int a=4, c=6, i=2;
         System.out.println("Test 1 ="+ (a+c));
+        if(i<=1) {
+        	System.out.println("Good Job");
+        }else if(i>1) {
+        	System.out.println("Bad Job");
+        }
 	}
 
 }
