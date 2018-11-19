960121db5f1349eb835bb54219f34207a6ee340e
Samuel <samueljimenezluis@gmail.com>
2018-11-19 10:50:56 +0100

    Merge arreglado

--- src/pruebaGitHub/Prueba.java
+++ src/pruebaGitHub/Prueba.java
@@ -4,11 +4,8 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-        int a=4, c=6, i=2;
-=======
-        int a=4, c=15;
->>>>>>> master
+		int a=4, c=6, i=2;
+
         System.out.println("Test 1 ="+ (a+c));
         if(i<=1) {
         	System.out.println("Good Job");
